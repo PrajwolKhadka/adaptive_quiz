89e62b682509005c127a05d6512096eb418b0e27
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-12-31 13:03:19 +0545

    Folder Structure and pubsec

---
+++ lib/features/splash/presentation/pages/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:adaptive_quiz/screens/login_screen.dart';
+import 'package:adaptive_quiz/screens/onboarding_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  get splash => null;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: SizedBox.expand(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFFFFFFFF),
+                Color(0xFFBEE1FA),
+              ],
+            ),
+          ),
+          child: Center(
+            child: Lottie.asset("assets/lottie/splash.json"),
+          ),
+        ),
+      ),
+      nextScreen: OnboardingScreen(),
+      splashIconSize: double.infinity,
+      backgroundColor: Colors.transparent,
+      duration: 2500,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:adaptive_quiz/app.dart';
+import 'package:adaptive_quiz/app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
