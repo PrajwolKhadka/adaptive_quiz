eef53ea8d89c84ed8cd5da0c3a17302492c04612
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-01-15 16:55:13 +0545

    entity update as per backend

--- lib/features/auth/domain/entities/auth_entity.dart
+++ lib/features/auth/domain/entities/auth_entity.dart
@@ -1,11 +1,22 @@
-class AuthEntity {
-  final String userId;
-  final String email;
-  final String password;
+import 'package:equatable/equatable.dart';
 
-  AuthEntity({
-    required this.userId,
+class AuthEntity extends Equatable {
+  final String? id;
+  final String fullName;
+  final String email;
+  final String className;
+  final bool isFirstLogin;
+  final String? token;
+
+  const AuthEntity({
+    this.id,
+    required this.fullName,
     required this.email,
-    required this.password,
+    required this.className,
+    required this.isFirstLogin,
+    this.token,
   });
+
+  @override
+  List<Object?> get props => [id, email, isFirstLogin];
 }
