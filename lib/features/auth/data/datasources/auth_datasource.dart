62e4ce1adb7fc6418cb386961477a892d8d4f12f
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-01-15 16:53:58 +0545

    testing datasource

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,16 +1,16 @@
-import 'package:adaptive_quiz/core/services/hive/hive_service.dart';
-import 'package:adaptive_quiz/features/auth/data/models/auth_hive_model.dart';
-
-abstract interface class IAuthDatasource {
-  AuthHiveModel? login(String email, String password);
-}
-
-class AuthLocalDatasource implements IAuthDatasource {
-  final HiveService hiveService;
-  AuthLocalDatasource(this.hiveService);
-
-  @override
-  AuthHiveModel? login(String email, String password) {
-    return hiveService.login(email, password);
-  }
-}
+// import 'package:adaptive_quiz/core/services/hive/hive_service.dart';
+// import 'package:adaptive_quiz/features/auth/data/models/auth_hive_model.dart';
+//
+// abstract interface class IAuthDatasource {
+//   AuthHiveModel? login(String email, String password);
+// }
+//
+// class AuthLocalDatasource implements IAuthDatasource {
+//   final HiveService hiveService;
+//   AuthLocalDatasource(this.hiveService);
+//
+//   @override
+//   AuthHiveModel? login(String email, String password) {
+//     return hiveService.login(email, password);
+//   }
+// }
