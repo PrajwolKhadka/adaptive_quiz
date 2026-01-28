071b8fe7ea332e91116a771436718c3ddab59c91
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-01-28 15:00:52 +0545

    auth response changes

--- lib/features/auth/domain/entities/auth_response.dart
+++ lib/features/auth/domain/entities/auth_response.dart
@@ -1,9 +1,39 @@
+// class AuthResponse {
+//   final String token;
+//   final bool isFirstLogin;
+//
+//   AuthResponse({
+//     required this.token,
+//     required this.isFirstLogin,
+//   });
+// }
+
 class AuthResponse {
-  final String token;
+  final String studentId;
+  final String fullName;
+  final String email;
+  final String className;
   final bool isFirstLogin;
+  final String token;
 
   AuthResponse({
-    required this.token,
+    required this.studentId,
+    required this.fullName,
+    required this.email,
+    required this.className,
     required this.isFirstLogin,
+    required this.token,
   });
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      studentId: json['studentId'],
+      fullName: json['fullName'],
+      email: json['email'],
+      className: json['className'],
+      isFirstLogin: json['isFirstLogin'],
+      token: json['token'],
+    );
+  }
 }
+
