0bc1e265c44837c2f1f1e19349eedea915d57320
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-02-25 12:47:07 +0545

    refactor(result): improve result view model logic

--- lib/features/dashboard/presentation/view_model/result_view_model.dart
+++ lib/features/dashboard/presentation/view_model/result_view_model.dart
@@ -1,3 +1,5 @@
+import 'package:adaptive_quiz/core/api/api_client.dart';
+import 'package:adaptive_quiz/core/providers/common_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../domain/usecases/result_usecases.dart';
 import '../providers/result_provider.dart';
@@ -7,9 +9,10 @@ class ResultViewModel extends Notifier<ResultState> {
   late final GetMyHistoryUsecase _getMyHistory;
   late final GetPerformanceGraphUsecase _getPerformanceGraph;
   late final GetMyResultDetailUsecase _getMyResultDetail;
-
+  late final ApiClient _apiClient;
   @override
   ResultState build() {
+    _apiClient = ref.read(apiClientProvider);
     _getMyHistory = ref.read(getMyHistoryUsecaseProvider);
     _getPerformanceGraph = ref.read(getPerformanceGraphUsecaseProvider);
     _getMyResultDetail = ref.read(getMyResultDetailUsecaseProvider);
